78388e270282cddc6b48267b27f26ee78e41163e
Hachi86Roku <ekukanic@gmail.com>
2024-12-05 21:18:57 +0000

    Adding the data frame to the test app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("🎈 My brand new super app")
-st.write(
-    "Let's start building Stuff! For help and inspiration, head over to nothing"
-)
+st.write("Let's start building Stuff! For help and inspiration, head over to nothing")
+
+st.divider()
+
+df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+st.dataframe(df)  # Same as st.write(df)
