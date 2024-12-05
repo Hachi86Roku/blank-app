9a2d5a52bfe4b73676c060f8bc8700f265654bd0
Hachi86Roku <ekukanic@gmail.com>
2024-12-05 20:58:15 +0000

    This is the new Commit text 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My brand new super app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building Stuff! For help and inspiration, head over to nothing"
 )
